8fd98bd4b09f3b2d5eb1d883b2e7afc32de115b0
isachenkoroma <isa-ro@yandex.ru>
2018-03-14 22:44:26 +0300

    qpfs changes

--- code/qpfs.py
+++ code/qpfs.py
@@ -10,20 +10,25 @@ class QPFS:
             raise ValueError('Similarity measure should be "corr" or "info"')
         self.sim = sim
     
-    def get_Qb(self, X, y):
+    def get_Qb(self, X, y, eps=1e-12):
 
         n_features = X.shape[1]
-
+        self.Q = np.ones([n_features, n_features])
+        
         if self.sim == 'corr':
             y_mat = y[:, np.newaxis] if len(y.shape) == 1 else y[:]
-            corr = np.corrcoef(np.hstack([X, y_mat]).T)
-            self.Q = np.abs(corr[:-1, :-1])
+            idxs_nz = np.where(np.sum(X ** 2, axis = 0) != 0)[0]
+            corr = np.corrcoef(np.hstack([X[:, idxs_nz], y_mat]).T)
+            for i, idx in enumerate(idxs_nz):
+                self.Q[idx, idxs_nz] = np.abs(corr[i, :-1])
             self.b = np.abs(corr[:-1, [-1]])
         elif self.sim == 'info':
-            self.Q = np.zeros([n_features, n_features])
             for j in range(n_features):
                 self.Q[:, j] = sklfs.mutual_info_regression(X, X[:, j])
             self.b = sklfs.mutual_info_regression(X, y)
+        
+        self.Q = np.nan_to_num(self.Q)
+        self.b = np.nan_to_num(self.b)
         
         self.lamb_min = sc.linalg.eigh(self.Q)[0][0]
         if self.lamb_min < 0:
@@ -31,7 +36,7 @@ class QPFS:
     
     def get_alpha(self, kind=0, **kwargs):
         if kind == 0:
-            return self.Q.mean() / (self.Q.mean() + self.b.mean())
+            return 2 * self.Q.mean() / self.b.mean()
         elif kind == 1:
             return self.Q.mean() * len(self.b) * kwargs['k'] / self.b.mean()
         else:
